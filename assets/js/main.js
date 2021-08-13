fbb3c226dd3f7ef33360f9670b19a31641a4dfdb
Yoshiyuki-izumoji <87624321+Yoshiyuki-izumoji@users.noreply.github.com>
2021-08-13 14:11:18 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,8 +21,6 @@ const app = Vue.createApp({
         },
         stage2: {
           q1: 'いいい',
-          q2: 'ううう',
-          q3: 'BBB',
         },
         stage3: {
           q1: 'えええ',
@@ -38,8 +36,6 @@ const app = Vue.createApp({
         ],
         stage2: [
           false, // 2-1
-          false, // 2-2
-          false, // 2-3
         ],
         stage3: [
           false,
